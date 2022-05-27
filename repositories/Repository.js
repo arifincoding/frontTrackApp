1467e9b8b1cce8497af089990a0aa41ea98d6165
arifincoding <arifinnakpojok@gmail.com>
2022-05-27 21:51:45 +0700

    delete customerrepo

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -7,7 +7,6 @@ import AuthRepo from '~/repositories/AuthRepo'
 import ChatRepo from '~/repositories/ChatRepo'
 import UserRepo from '~/repositories/UserRepo'
 import HistoryRepo from '~/repositories/HistoryRepo'
-import CustomerRepo from '~/repositories/CustomerRepo'
 
 export default ($axios,store) => ({
     auth: AuthRepo($axios, store),
@@ -18,6 +17,5 @@ export default ($axios,store) => ({
     service: ServiceRepo($axios, store),
     chat: ChatRepo($axios, store),
     user: UserRepo($axios, store),
-    history: HistoryRepo($axios, store),
-    customer: CustomerRepo($axios,store)
+    history: HistoryRepo($axios, store)
 })
